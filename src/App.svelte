9ffc95f43a6b28c41c99187f85d1291443723ec0
ge49cum <ge49cum@mytum.de>
2022-05-27 15:38:02 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+    import Model from './components/Model.svelte';
+    import SlideInPanel from './components/SlideInPanel.svelte'
+    import { fade,fly } from 'svelte/transition';
+
+
+    let anzeigen=false;
+    let linkInfo;
+    let item;
+
+    const toggleSidebar = (e) => {
+		console.log(e.detail)
+		if(e.detail!=null){
+		anzeigen=true;
+		//console.log(e.detail);
+		linkInfo=null
+		item=e.detail;
+		}
+		else{
+			//anzeigen=false;
+		}
+	}
+
+    const toggleLinkSidebar = (e) => {
+		anzeigen=true;
+		linkInfo=e.detail;
+		item=null;
+		//console.log(linkInfo)
+	}
+
+    const toggleClose = () => {
+		anzeigen=false;
+	}
+</script>
+
+<Model on:showPanel={toggleSidebar} on:showPanelForLinks={toggleLinkSidebar}/>
+{#if anzeigen}
+<div transition:fade>
+	<SlideInPanel {linkInfo} {item} on:click={toggleClose}/>
+</div>
+{/if}
---
+++ src/components/SlideInPanel.svelte
@@ -0,0 +1,97 @@
+<script>
+    export let item;
+    export let linkInfo;
+    import { BarLoader } from 'svelte-loading-spinners'
+
+</script>
+
+
+{#if item!=null}
+<div class="overlay-content">
+ <button class="close" on:click>X</button>
+ <h1>{item.name}</h1>
+ <ul>
+   <li class="flex">
+     <div class="col-1" >Kurzfrom</div>
+     <div class="col-2" >{item.kuerzel}</div>
+ </li>
+ <li class="flex">
+   <div class="col-1" >Webseite</div>
+   <div class="col-2" >{item.webseite}</div>
+</li>
+ <li class="flex">
+     <div class="col-1" >Ebene</div>
+     <div class="col-2" >{item.ebene}</div>
+ </li>
+</ul>
+</div> 
+{/if} 
+{#if linkInfo!=null}
+<div class="overlay-content">
+ <button class="close" on:click>X</button>
+ <h1>{linkInfo.node1.kuerzel} und {linkInfo.node2.kuerzel}</h1>
+</div>  
+{/if}
+
+ 
+
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200&display=swap');
+
+.overlay-content {
+ width: 30%;
+ height: 100%;
+ background-color: #EDF1EA;
+ color: #080808;
+ align-content: center;
+ /* display: flex; */
+ overflow: hidden;
+ position: absolute; 
+ /* z-index: 1; */
+ transition: 0.6s;
+ top: 0%;
+ left: 100%;
+ opacity: 1;
+ margin-left: auto;
+ margin-right: auto;
+ padding-top: 50px;
+ opacity: 1;
+
+}
+.overlay-content {
+ transform: translateX(-100%);
+}
+
+
+h1{
+ font-family: 'Source Code Pro', monospace;
+ top: 0px;
+ text-align: center;
+ letter-spacing: 1px;
+ margin-bottom: 50px;
+}
+.flex{
+   border-radius: 3px;
+   padding: 25px 30px;
+   display: flex;
+   justify-content: space-between;
+   margin: 30px 12.5px;
+   background-color: #ffffff;
+   box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.1);
+}
+.col-1 {
+   flex-basis: 10%;
+ }
+.col-2 {
+   flex-basis: 25%;
+ }
+.close{
+ position: absolute;
+ top: 15px;
+ left: 15px;
+ border: none;
+ cursor: pointer;
+}
+</style>

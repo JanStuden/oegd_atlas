ae533b752d9c6f92264207ba404311d1d51bc6ba
ge49cum <ge49cum@mytum.de>
2022-05-28 12:13:41 +0200

    backend

--- server/db/database.js
+++ server/db/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     //db: 'mongodb://localhost:27017/oegd_atlas'
-    db: "mongodb+srv://JanStud:oegd_atlas_pwd@cluster1.gcgjaxi.mongodb.net/?retryWrites=true&w=majority/oegd_atlas"
+    db: "mongodb+srv://JanStud:blabla@cluster1.gcgjaxi.mongodb.net/?retryWrites=true&w=majority/oegd_atlas"
   };
